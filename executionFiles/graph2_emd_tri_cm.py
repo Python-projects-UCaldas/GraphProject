a0a5a7e31c51a0fc7db9d804f07cbe5c8d6550bd
MarcoContreras23 <marcocontreras2309@gmail.com>
2021-11-14 18:09:42 -0500

    Graphs

---
+++ executionFiles/graph2_emd_tri_cm.py
@@ -0,0 +1,22 @@
+import os
+import pyphi
+import numpy as np
+import json
+
+f = open(os.path.dirname(__file__) + "/../graphs/graph_2.json")
+data = json.load(f)
+
+pyphi.config.load_file(
+    os.path.dirname(__file__) + "/../config/pyphi_config_emd_tri.yml"
+)
+
+network = pyphi.Network(data['tpm'], cm=data['cm'], node_labels=data['labels'])
+state = data['state']
+node_indices = (0, 1, 2, 3)
+subsystem = pyphi.Subsystem(network, state, node_indices)
+f.close()
+sia = pyphi.compute.sia(subsystem)
+
+print("MIP: \n", sia.cut)
+print("Phi: \n Φ = ", sia.phi)
+print("Tiempo: \n", sia.time, "s")
